23d94a96f715451d156970f6232b9606e181b4ce
Eric Kibogo <erickibogo216@gmail.com>
2020-10-12 23:47:50 +0300

    Add validity checker to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,15 @@ $(document).ready(function(){
      $("#dev-info-showing").toggle();
  }); 
  
+ $("form#form").on('submit',function(event){
+    event.preventDefault();
+    let name = $("input#name").val();
+    let email = $("input#email").val();
+    let message = $("textarea#message").val();
+
+    if ($("input#name").val() && $("input#email").val()){
+        alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you soon!");
+    }
+    else {
+        alert("Please provide a valid name and email!");
  })
