44edb2a59a4c609ad885804ac30894362b610eab
Eric Kibogo <erickibogo216@gmail.com>
2020-10-12 23:27:52 +0300

    Add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-$(document).ready(
-    
-)
+
+$(document).ready(function(){
+
+    $("#portfoliopic").hover(function(){
+        $(".portfoliotext",this).Toggle();
+     }, function(){
+        $(".portfoliotext",this).Toggle();
+     });
+
+    $("#clickable-design").click(function(){
+        $("#design-info-hidden").toggle();
+        $("#design-info-showing").toggle();
+        $
+    }); 
+ 
+    $("#clickable-prod").click(function(){
+     $("#prod-info-hidden").toggle();
+     $("#prod-info-showing").toggle();
+ });
+ 
+ $("#clickable-dev").click(function(){
+     $("#dev-info-hidden").toggle();
+     $("#dev-info-showing").toggle();
+ }); 
+ 
+ })
