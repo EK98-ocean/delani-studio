a7467a944d51f6c5aa3a974d2e0e09e74e80be26
Eric Kibogo <erickibogo216@gmail.com>
2020-10-20 16:07:13 +0300

    Amend scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,37 @@
 
 $(document).ready(function(){
+    $('.image-frame').hover(function(){
+            $('.image-caption', this).slideToggle('slow');
+        }, function(){
+            $('.image-caption', this).slideToggle('slow');
+    });
 
-$("#portfoliopic").hover(function(){
-    $(".portfoliotext",this).Toggle();
-}, function(){
-$(".portfoliotext",this).Toggle();
-});
+    $(".clickable-design").click(function(){
+        $(".design-info-hidden").toggle();
+        $(".design-info-showing").toggle();
+    }); 
+    
+    $(".clickable-prod").click(function(){
+        $(".prod-info-hidden").toggle();
+        $(".prod-info-showing").toggle();
+    });
+    
+    $(".clickable-dev").click(function(){
+        $(".dev-info-hidden").toggle();
+        $(".dev-info-showing").toggle();
+    }); 
 
-$("#clickable-design").click(function(){
-    $("#design-info-hidden").toggle();
-    $("#design-info-showing").toggle();
-}); 
- 
-$("#clickable-prod").click(function(){
-    $("#prod-info-hidden").toggle();
-     $("#prod-info-showing").toggle();
-});
- 
-$("#clickable-dev").click(function(){
-     $("#dev-info-hidden").toggle();
-     $("#dev-info-showing").toggle();
-}); 
+    $("form#form").on('submit',function(event){
+        event.preventDefault();
+        let name = $("input#name").val();
+        let email = $("input#email").val();
+        let message = $("textarea#message").val();
 
-$("form#form").on('submit',function(event){
-    event.preventDefault();
-    let name = $("input#name").val();
-    let email = $("input#email").val();
-    let message = $("textarea#message").val();
-
-    if ($("input#name").val() && $("input#email").val()){
-        alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you soon!");
-    }
-    else {
-        alert("Please provide a valid name and email!");
- })
+        if ($("input#name").val() && $("input#email").val()){
+            alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you soon!");
+        }
+        else {
+            alert("Please provide a valid name and email!");
+    };
+    });
+})
